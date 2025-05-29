8523b797bc0b6877012f8d0f01dd61dccbec66bc
jj <jjpreneur2@gmail.com>
2025-05-29 14:21:05 +0800

    Fix contact forms bug

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,10 @@
-const nameFieldEl = document.querySelector(".contact-page .name-input");
-const emailFieldEl = document.querySelector(".contact-page .email-input");
-const messageFieldEl = document.querySelector(".contact-page .message-input");
-const submitButtonEl = document.querySelector(".contact-page .submit-input");
+// const nameFieldEl = document.querySelector(".contact-page .name-input");
+// const emailFieldEl = document.querySelector(".contact-page .email-input");
+// const messageFieldEl = document.querySelector(".contact-page .message-input");
+// const submitButtonEl = document.querySelector(".contact-page .submit-input");
 
-submitButtonEl.addEventListener("click", () => {
-    if ((nameFieldEl.value) !== "" && (emailFieldEl.value) !== "" && (messageFieldEl.value) !== "") {
-        alert("Info submitted successfully!");
-    }
-})
+// submitButtonEl.addEventListener("click", () => {
+//     if ((nameFieldEl.value) !== "" && (emailFieldEl.value) !== "" && (messageFieldEl.value) !== "") {
+//         alert("Info submitted successfully!");
+//     }
+// })
