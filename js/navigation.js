b198202349d421924e08f907ce2693c5cd7c8770
jj <jjpreneur2@gmail.com>
2025-05-30 12:32:01 +0800

    Fix nav bar indicator bug

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,6 @@
 // Update style nav links after it reloads
 const links = document.querySelectorAll(".nav__link");
-const currentPage = location.pathname.split("/").pop();
+const currentPage = window.location.pathname.split("/").pop();
 links.forEach((link) => {
     if (link.getAttribute("href") === currentPage) { link.classList.add("active"); }
     else { link.classList.remove("active"); }
